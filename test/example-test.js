982e6992fc243157dd5ca869f86e540379ec1895
Carsten Wentzlow <carsten@marble.(none)>
2011-12-09 20:16:38 +0100

    - adding blowfish test

--- test/example-test.js
+++ test/example-test.js
@@ -3,6 +3,6 @@ unittests.register("Example test", function() {
 	var result = new Array();
 	
 	result[0] = new test_result("test1 - testing on (\"str\" == \"str\")", ("str" == "str"));
-	result[1] = new test_result("test2 - testing on (1 == 1)", (1 == 1));
+	result[1] = new test_result("test2 - testing on (1 == 2)", (1 == 2));
 	return result;
 });
