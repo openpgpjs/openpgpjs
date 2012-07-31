3a092ce6821ea4f769d29dbe84318e2b0e2c4309
Sean Colyer <sean@colyer.name>
2012-07-30 20:43:41 -0400

    Change to ignore case when looking up keys. Change to transform both input and keyring keys to use .toLowerCase(). Prompted by: https://github.com/seancolyer/gmail-crypt/issues/12

--- resources/openpgp.js
+++ resources/openpgp.js
@@ -10515,12 +10515,13 @@ function openpgp_keyring() {
 		} else {
 			email = email_address.trim();
 		}
+		email = email.toLowerCase();
 		if(!util.emailRegEx.test(email)){
 		    return results;
 		}
 		for (var i =0; i < this.publicKeys.length; i++) {
 			for (var j = 0; j < this.publicKeys[i].obj.userIds.length; j++) {
-				if (this.publicKeys[i].obj.userIds[j].text.indexOf(email) >= 0)
+				if (this.publicKeys[i].obj.userIds[j].text.toLowerCase().indexOf(email) >= 0)
 					results[results.length] = this.publicKeys[i];
 			}
 		}
@@ -10542,12 +10543,13 @@ function openpgp_keyring() {
 		} else {
 			email = email_address.trim();
 		}
+		email = email.toLowerCase();
 		if(!util.emailRegEx.test(email)){
 		    return results;
 		}
 		for (var i =0; i < this.privateKeys.length; i++) {
 			for (var j = 0; j < this.privateKeys[i].obj.userIds.length; j++) {
-				if (this.privateKeys[i].obj.userIds[j].text.indexOf(email) >= 0)
+				if (this.privateKeys[i].obj.userIds[j].text.toLowerCase().indexOf(email) >= 0)
 					results[results.length] = this.privateKeys[i];
 			}
 		}
