9ad5116d3c2695402099729fdfbd9fdac3f62cc8
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-01-10 18:31:13 +0100

    show sent message

--- resources/example.js
+++ resources/example.js
@@ -2,4 +2,5 @@ function encrypt() {
   openpgp.init();
   var pub_key = openpgp.read_publicKey($('#pubkey').text());
   $('#message').val(openpgp.write_encrypted_message(pub_key,$('#message').val()));
+  window.alert("This message is going to be sent:\n" + $('#message').val());
 }
