3680695a69a6e0f2ab6ff7e5954bc6459dd5696f
Robert Nelson <robertn@the-nelsons.org>
2014-01-09 02:29:30 -0800

    Implement node.js support and cleanup source tree.
    Rename resources to dist.
    
    Eliminate keyring bundle and expose keyring class in openpgp module.
    
    Add mochaTest grunt task to run node server-side tests.
    
    Add node_pack grunt task to create npm package into dist and install it for testing.
    
    Add node_store config property which specifies location of localStorage emulation when using node.
    
    Add repository info to package.json.
    
    Move util.js to src directory from util since it is the only file there.
    
    Rename class properties in openpgp to the new class names.

---
+++ example/example.php
@@ -0,0 +1,30 @@
+<?php
+// Author: Alex
+// Description: OpenPGP.js message example
+
+/* setup -------------------------------------------------------------------- */
+define('SMALL_LIMIT',128);
+define('LARGE_LIMIT',131072);
+$subject="OpenPGP.js Example";
+$to="alex@openpgpjs.org";
+$headers = "From: OpenPGP.js Example <noreply@openpgpjs.org>\r\n";
+$redirect = "http://openpgpjs.org/";
+/* -------------------------------------------------------------------------- */
+
+/* input -------------------------------------------------------------------- */
+$user=substr(trim($_POST["mail"]), 0, SMALL_LIMIT);
+$message=substr(trim($_POST["message"]), 0, LARGE_LIMIT);
+/* -------------------------------------------------------------------------- */
+
+/* send the request --------------------------------------------------------- */
+if (filter_var($user, FILTER_VALIDATE_EMAIL)) {
+    $headers = "From: $user\r\n";
+}
+if (!empty($message))
+  mail($to, $subject, $message, $headers);
+/* -------------------------------------------------------------------------- */
+
+/* Redirect browser */
+header("Location: " . $redirect);
+/* -------------------------------------------------------------------------- */
+?>
