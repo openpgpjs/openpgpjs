a4598f3bd4a7725c645841c736ee17e6d04073e6
Thomas Oberndörfer <toberndo@yarkon.de>
2014-03-31 16:09:23 +0200

    Generate openpgp.js without source map. New source map target is openpgp_debug.js. Change dependency of workers: openpgp.worker.js -> openpgp.js, openpgp.worker.min.js -> openpgp.min.js. Remove openpgp.js with maps from unittests-bundle.js

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -17,7 +17,7 @@
 
 window = {}; // to make UMD bundles work
 
-importScripts('openpgp.min.js');
+importScripts('openpgp.js');
 
 var MIN_SIZE_RANDOM_BUFFER = 40000;
 var MAX_SIZE_RANDOM_BUFFER = 60000;
