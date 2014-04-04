82b18c61f3f1bdc0770edc2af5d8a1cbe50a1f5e
Thomas Oberndörfer <toberndo@yarkon.de>
2014-04-04 17:33:30 +0200

    Add empty read method to trust packet. Otherwise import of key with a trust packet will fail. Trust packets are ignored and filtered out by the Key class.

--- src/packet/trust.js
+++ src/packet/trust.js
@@ -13,3 +13,12 @@ var enums = require('../enums.js');
 function Trust() {
   this.tag = enums.packet.trust;
 }
+
+/**
+ * Parsing function for a trust packet (tag 12).
+ * Currently empty as we ignore trust packets
+ * @param {String} byptes payload of a tag 12 packet
+ */
+Trust.prototype.read = function (bytes) {
+
+};
