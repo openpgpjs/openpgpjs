da650e2d634cf4a375e57948239f4c38088d2ddf
Christian Zangl <coralllama@gmail.com>
2014-12-01 23:39:21 +0100

    add compression support (for zip & zlib) using https://github.com/imaya/zlib.js add zlibjs dependency, grunt task fix compData vs compdata bug

--- src/packet/compressed.js
+++ src/packet/compressed.js
@@ -21,17 +21,21 @@
  * {@link http://tools.ietf.org/html/rfc4880#section-5.6|RFC4880 5.6}: The Compressed Data packet contains compressed data.  Typically,
  * this packet is found as the contents of an encrypted packet, or following
  * a Signature or One-Pass Signature packet, and contains a literal data packet.
- * @requires compression/jxg
- * @requires encoding/base64
+ * @requires compression/zlib
+ * @requires compression/rawinflate
+ * @requires compression/rawdeflate
  * @requires enums
+ * @requires util
  * @module packet/compressed
  */
 
 module.exports = Compressed;
 
 var enums = require('../enums.js'),
-  JXG = require('../compression/jxg.js'),
-  base64 = require('../encoding/base64.js');
+  util = require('../util.js'),
+  Zlib = require('../compression/zlib.min.js'),
+  RawInflate = require('../compression/rawinflate.min.js'),
+  RawDeflate = require('../compression/rawdeflate.min.js');
 
 /**
  * @constructor
@@ -51,7 +55,7 @@ function Compressed() {
    * Compression algorithm
    * @type {compression}
    */
-  this.algorithm = 'uncompressed';
+  this.algorithm = 'zip';
 
   /**
    * Compressed packet data
@@ -93,7 +97,7 @@ Compressed.prototype.write = function () {
  * read by read_packet
  */
 Compressed.prototype.decompress = function () {
-  var decompressed, compdata, radix;
+  var decompressed;
 
   switch (this.algorithm) {
     case 'uncompressed':
@@ -101,35 +105,13 @@ Compressed.prototype.decompress = function () {
       break;
 
     case 'zip':
-      compData = this.compressed;
-
-      radix = base64.encode(compData).replace(/\n/g, "");
-      // no header in this case, directly call deflate
-      var jxg_obj = new JXG.Util.Unzip(JXG.Util.Base64.decodeAsArray(radix));
-
-      decompressed = unescape(jxg_obj.deflate()[0][0]);
+      var inflate = new RawInflate.Zlib.RawInflate(util.str2Uint8Array(this.compressed));
+      decompressed = util.Uint8Array2str(inflate.decompress());
       break;
 
     case 'zlib':
-      //RFC 1950. Bits 0-3 Compression Method
-      var compressionMethod = this.compressed.charCodeAt(0) % 0x10;
-
-      //Bits 4-7 RFC 1950 are LZ77 Window. Generally this value is 7 == 32k window size.
-      // 2nd Byte in RFC 1950 is for "FLAGs" Allows for a Dictionary
-      // (how is this defined). Basic checksum, and compression level.
-
-      if (compressionMethod == 8) { //CM 8 is for DEFLATE, RFC 1951
-        // remove 4 bytes ADLER32 checksum from the end
-        compData = this.compressed.substring(0, this.compressed.length - 4);
-        radix = base64.encode(compData).replace(/\n/g, "");
-        //TODO check ADLER32 checksum
-        decompressed = JXG.decompress(radix);
-        break;
-
-      } else {
-        throw new Error("Compression algorithm ZLIB only supports " +
-          "DEFLATE compression method.");
-      }
+      var inflate = new Zlib.Zlib.Inflate(util.str2Uint8Array(this.compressed));
+      decompressed = util.Uint8Array2str(inflate.decompress());
       break;
 
     case 'bzip2':
@@ -147,21 +129,27 @@ Compressed.prototype.decompress = function () {
  * Compress the packet data (member decompressedData)
  */
 Compressed.prototype.compress = function () {
+  var uncompressed, deflate;
+  uncompressed = this.packets.write();
+
   switch (this.algorithm) {
 
     case 'uncompressed':
       // - Uncompressed
-      this.compressed = this.packets.write();
+      this.compressed = uncompressed;
       break;
 
     case 'zip':
       // - ZIP [RFC1951]
-      throw new Error("Compression algorithm ZIP [RFC1951] is not implemented.");
+      deflate = new RawDeflate.Zlib.RawDeflate(util.str2Uint8Array(uncompressed));
+      this.compressed = util.Uint8Array2str(deflate.compress());
+      break;
 
     case 'zlib':
       // - ZLIB [RFC1950]
-      // TODO: need to implement this
-      throw new Error("Compression algorithm ZLIB [RFC1950] is not implemented.");
+      deflate = new Zlib.Zlib.Deflate(util.str2Uint8Array(uncompressed));
+      this.compressed = util.Uint8Array2str(deflate.compress());
+      break;
 
     case 'bzip2':
       //  - BZip2 [BZ2]
