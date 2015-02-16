c272dc86634f3beac136bdabb820eadead2b9111
Arturo Filastò <art@fuffa.org>
2015-02-16 18:17:23 +0100

    Run the stream unittests as part of the test suite

---
+++ test/stream/index.js
@@ -0,0 +1,6 @@
+describe('Stream', function () {
+  require('./crypto.js');
+  require('./message.js');
+  require('./packet.js');
+});
+
--- test/unittests.js
+++ test/unittests.js
@@ -1,6 +1,7 @@
 describe('Unit Tests', function () {
   require('./general');
   require('./crypto');
+  require('./stream');
   if (typeof window !== 'undefined') {
     require('./worker');
   }
