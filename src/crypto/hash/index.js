f56b6887d9296365d701da079d13c7aca91fd88a
Bart Butler <bart.c.butler@gmail.com>
2015-05-01 12:41:35 -0700

    native crypto and hash for nodeJS

--- src/crypto/hash/index.js
+++ src/crypto/hash/index.js
@@ -1,32 +1,74 @@
 /**
  * @requires crypto/hash/sha
+ * @requires crypto/hash/rusha
+ * @requires util
+ * @requires config
  * @module crypto/hash
  */
 var sha = require('./sha.js'),
-  forge_sha256 = require('./forge_sha256.js'),
   rusha = require('./rusha.js'),
+  config = require('../../config')
   util = require('../../util.js');
 
 var rusha_obj = new rusha();
 
+function node_hash(type) {
+  return function (data) {
+    var nodeCrypto = require('crypto');
+    var Buffer = require('buffer').Buffer;
+    var shasum = nodeCrypto.createHash(type);
+    shasum.update(new Buffer(data));
+    return new Uint8Array(shasum.digest());
+  }
+}
+
+var hash_fns;
+if(typeof module !== 'undefined' && module.exports && config.useNative) { // Use Node native crypto
+  hash_fns = {
+    md5: node_hash('md5'),
+    sha1: node_hash('sha1'),
+    sha224: node_hash('sha224'),
+    sha256: node_hash('sha256'),
+    sha384: node_hash('sha384'),
+    sha512: node_hash('sha512'),
+    ripemd: node_hash('ripemd160')
+  };
+}
+else { // JS
+  hash_fns = {
+    /** @see module:crypto/hash/md5 */
+    md5: require('./md5.js'),
+    /** @see module:crypto/hash/sha.sha1 */
+    /** @see module:crypto/hash/rusha */
+    // sha1: sha.sha1,
+    sha1: function (data) {
+      return util.str2Uint8Array(util.hex2bin(rusha_obj.digest(data)));
+    },
+    //sha1: asmCrypto.SHA1.bytes,
+    /** @see module:crypto/hash/sha.sha224 */
+    sha224: sha.sha224,
+    /** @see module:crypto/hash/sha.sha256 */
+    /** @see module:crypto/asmcrypto */
+    //sha256: sha.sha256,
+    sha256: asmCrypto.SHA256.bytes,
+    /** @see module:crypto/hash/sha.sha384 */
+    sha384: sha.sha384,
+    /** @see module:crypto/hash/sha.sha512 */
+    sha512: sha.sha512,
+    /** @see module:crypto/hash/ripe-md */
+    ripemd: require('./ripe-md.js')
+  };
+}
+
 module.exports = {
-  /** @see module:crypto/hash/md5 */
-  md5: require('./md5.js'),
-  /** @see module:crypto/hash/sha.sha1 */
-  sha1: sha.sha1,
-  sha1: function (data) {
-    return util.str2Uint8Array(util.hex2bin(rusha_obj.digest(data)));
-  },
-  /** @see module:crypto/hash/sha.sha224 */
-  sha224: sha.sha224,
-  /** @see module:crypto/hash/sha.sha256 */
-  sha256: sha.sha256,
-  /** @see module:crypto/hash/sha.sha384 */
-  sha384: sha.sha384,
-  /** @see module:crypto/hash/sha.sha512 */
-  sha512: sha.sha512,
-  /** @see module:crypto/hash/ripe-md */
-  ripemd: require('./ripe-md.js'),
+
+  md5: hash_fns.md5,
+  sha1: hash_fns.sha1,
+  sha224: hash_fns.sha224,
+  sha256: hash_fns.sha256,
+  sha384: hash_fns.sha384,
+  sha512: hash_fns.sha512,
+  ripemd: hash_fns.ripemd,
 
   /**
    * Create a hash on the specified data using the specified algorithm
@@ -47,11 +89,7 @@ module.exports = {
         return this.ripemd(data);
       case 8:
         // - SHA256 [FIPS180]
-        //return this.sha256(data);
-
-        var sha256 = forge_sha256.create();
-        sha256.update(util.Uint8Array2str(data));
-        return util.str2Uint8Array(sha256.digest().getBytes());
+        return this.sha256(data);
       case 9:
         // - SHA384 [FIPS180]
         return this.sha384(data);
