c38d41036e4b2c2deb01695daf0c79675b75db7b
Tankred Hase <mail@tankredhase.de>
2016-02-08 15:02:02 +0700

    Add sinon to devDeps for mocking

--- test/unittests.js
+++ test/unittests.js
@@ -1,3 +1,11 @@
+(typeof window !== 'undefined' ? window : global).resolves = function(val) {
+  return new Promise(function(res) { res(val); });
+};
+
+(typeof window !== 'undefined' ? window : global).rejects = function(val) {
+  return new Promise(function(res, rej) { rej(val); });
+};
+
 describe('Unit Tests', function () {
   require('./general');
   require('./crypto');
