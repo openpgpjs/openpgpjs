42e11715897503e497681497313a1dc1c1827cb1
Tankred Hase <mail@tankredhase.de>
2016-06-08 11:34:56 +0200

    Add support for old-version releases

--- release.sh
+++ release.sh
@@ -12,6 +12,7 @@ if [ "$#" -ne 1 ] ; then
 fi
 
 # install dependencies
+rm -rf node_modules
 npm install
 
 # set version
@@ -30,4 +31,4 @@ git push
 git push --tag
 
 # publish to npm
-npm publish
+npm publish --tag old-version
