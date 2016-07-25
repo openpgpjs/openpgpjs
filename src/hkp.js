9718ff467929097879b1110ecc9b59ad7b57a02c
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2016-07-25 23:14:48 +0200

    functionality to search for keys with hkp

--- src/hkp.js
+++ src/hkp.js
@@ -37,6 +37,55 @@ export default function HKP(keyServerBaseUrl) {
 
 /**
  * Search for a public key on the key server either by key ID or part of the user ID.
+ * @param  {String}   options.query   This can be any part of the key user ID such as name
+ *   or email address.
+ * @return {Array} An array of key IDs, fingerprints, usernames, mail addresses and key IDs
+ */
+HKP.prototype.search = function (options) {
+  var uri = this._baseUrl + '/pks/lookup?op=index&options=mr&search=',
+      fetch = this._fetch;
+
+  if (options.query) {
+    uri += encodeURIComponent(options.query);
+  } else {
+    throw new Error('You must provide a query parameter!');
+  }
+
+  return fetch(uri).then(function (response) {
+    var result;
+    if (response.status === 404) {
+      return "";
+    } else if (response.status === 200) {
+      result = response.text();
+      return result;
+    }
+  }).then(function(uids){
+    var result, uidary, values;
+    uidary = uids.match(/pub:(.|\nuid)*/gm);
+    result = {};
+    for (var i in uidary) {
+      var keyID;
+      values = uidary[i].match(/pub:.*/)[0].split(":");
+      keyID = values[1].substr(24,16);
+      result[keyID] = {};
+      result[keyID].fingerprint = values[1];
+      result[keyID].algo = values[2];
+      result[keyID].keylength = values[3];
+      result[keyID].created = new Date(parseInt(values[4])*1000);
+      result[keyID].valid = new Date(parseInt(values[5])*1000);
+
+      result[keyID].uids = [];
+      values = uidary[i].match(/uid:.*/g);
+      for (var n in values) {
+        result[keyID].uids.push(values[n].split(":")[1]);
+      }
+    };
+    return result;
+  });
+};
+
+/**
+ * Search for a public key on the key server either by key ID or part of the user ID.
  * @param  {String}   options.keyID   The long public key ID.
  * @param  {String}   options.query   This can be any part of the key user ID such as name
  *   or email address.
