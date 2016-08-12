c54a12f2031870e31875dc74be5df925ae225661
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2016-08-12 00:24:22 -0300

    Signature creation and verification

---
+++ src/crypto/public_key/elliptic/ecdsa.js
@@ -0,0 +1,66 @@
+// OpenPGP.js - An OpenPGP implementation in javascript
+// Copyright (C) 2015-2016 Decentral
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+// Implementation of ECDSA following RFC6637 for Openpgpjs
+
+/**
+ * @requires crypto/public_key/jsbn
+ * @requires crypto/public_key/elliptic/curves
+ * @module crypto/public_key/elliptic/ecdsa
+ */
+
+'use strict';
+
+import curves from './curves.js';
+import BigInteger from '../jsbn.js';
+
+/**
+ * Sign a message using the provided key
+ * @param  {String}      oid        Elliptic curve for the key
+ * @param  {enums.hash}  hash_algo  Hash algorithm used to sign
+ * @param  {Uint8Array}  m          Message to sign
+ * @param  {BigInteger}  w          Private key used to sign
+ * @return {{r: BigInteger, s: BigInteger}}  Signature of the message
+ */
+function sign(oid, hash_algo, m, w) {
+  const curve = curves.get(oid);
+  const key = curve.keyFromPrivate(w.toByteArray());
+  const signature = key.sign(m, hash_algo);
+  return {
+    r: new BigInteger(signature.r),
+    s: new BigInteger(signature.s)
+  };
+}
+
+/**
+ * Verifies if a signature is valid for a message
+ * @param  {String}      oid        Elliptic curve for the key
+ * @param  {enums.hash}  hash_algo  Hash algorithm used in the signature
+ * @param  {{r: BigInteger, s: BigInteger}}  signature  Signature to verify
+ * @param  {Uint8Array}  m          Message to verify
+ * @param  {BigInteger}  gw         Public key used to verify the message
+ */
+function verify(oid, hash_algo, signature, m, gw) {
+  const curve = curves.get(oid);
+  const key = curve.keyFromPublic(gw.toByteArray());
+  return key.verify(m, {r: signature.r.toByteArray(), s: signature.s.toByteArray()}, hash_algo);
+}
+
+module.exports = {
+  sign: sign,
+  verify: verify
+};
