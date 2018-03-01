30bd6102d5d4278bbbb47074ef51c3b344a3288c
Mahrud Sayrafi <mahrud@berkeley.edu>
2018-03-01 11:35:16 -0800

    OID's stored as Uint8Array

--- test/general/oid.js
+++ test/general/oid.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect;
 
 describe('Oid tests', function() {
   const OID = openpgp.OID;
+  const util = openpgp.util;
   const p256_oid = new Uint8Array([0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07]);
   const p384_oid = new Uint8Array([0x2B, 0x81, 0x04, 0x00, 0x22]);
   const p521_oid = new Uint8Array([0x2B, 0x81, 0x04, 0x00, 0x23]);
@@ -14,7 +15,7 @@ describe('Oid tests', function() {
       expect(oid).to.exist;
       expect(oid.oid).to.exist;
       expect(oid.oid).to.have.length(data.length);
-      expect(oid.oid).to.equal(openpgp.util.Uint8Array_to_str(data));
+      expect(oid.toHex()).to.equal(util.Uint8Array_to_hex(data));
     });
   });
   it('Reading and writing', function() {
@@ -25,12 +26,14 @@ describe('Oid tests', function() {
       expect(oid.read(data)).to.equal(data.length);
       expect(oid.oid).to.exist;
       expect(oid.oid).to.have.length(data.length-1);
-      expect(oid.oid).to.equal(openpgp.util.Uint8Array_to_str(data.subarray(1)));
+      expect(oid.toHex()).to.equal(util.Uint8Array_to_hex(data.subarray(1)));
       const result = oid.write();
       expect(result).to.exist;
       expect(result).to.have.length(data.length);
       expect(result[0]).to.equal(data.length-1);
-      expect(openpgp.util.Uint8Array_to_str(result.subarray(1))).to.equal(openpgp.util.Uint8Array_to_str(data.subarray(1)));
+      expect(
+        util.Uint8Array_to_hex(result.subarray(1))
+      ).to.equal(util.Uint8Array_to_hex(data.subarray(1)));
     });
   });
 });
