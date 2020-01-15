81d6b45ba83ff77f98337e8faca4fb12f632f2e3
Daniel Huigens <d.huigens@protonmail.com>
2020-01-15 14:40:56 +0100

    Test loading OpenPGP.js from a Worker in the application

---
+++ test/worker/application_worker.js
@@ -0,0 +1,34 @@
+/* globals tryTests: true */
+
+const chai = require('chai');
+
+const { expect } = chai;
+
+tryTests('Application Worker', tests, {
+  if: typeof window !== 'undefined' && window.Worker && window.MessageChannel
+});
+
+function tests() {
+
+  it('Should support loading OpenPGP.js from inside a Web Worker', async function() {
+    const worker = new Worker('./worker/worker_example.js');
+    async function delegate(action, message) {
+      return new Promise((resolve, reject) => {
+        const channel = new MessageChannel();
+        channel.port1.onmessage = function({ data }) {
+          if (data.error !== undefined) {
+            reject(new Error(data.error));
+          } else {
+            resolve(data.result);
+          }
+        };
+        worker.postMessage({ action, message }, [channel.port2]);
+      });
+    }
+    const encrypted = await delegate('encrypt', 'Hello World!');
+    const decrypted = await delegate('decrypt', encrypted);
+    expect(decrypted).to.equal('Hello World!');
+    worker.terminate();
+  });
+
+}
