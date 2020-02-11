08d6b276e3fb54b8969bcae571fe90c2e8082c52
Daniel Huigens <d.huigens@protonmail.com>
2020-02-11 21:27:21 +0100

    Return data as string in openpgp.verify by default
    Add format='utf8'/'binary' param to openpgp.verify in order to be able
    to return binary data instead.

--- test/general/openpgp.js
+++ test/general/openpgp.js
@@ -2265,7 +2265,8 @@ describe('OpenPGP.js public api tests', function() {
           };
           const verifyOpt = {
             publicKeys: publicKey_2038_2045.keys,
-            date: future
+            date: future,
+            format: 'binary'
           };
           return openpgp.sign(signOpt).then(async function (signed) {
             verifyOpt.message = openpgp.message.fromBinary(data);
@@ -2289,7 +2290,8 @@ describe('OpenPGP.js public api tests', function() {
             armor: false
           };
           const verifyOpt = {
-            publicKeys: publicKey.keys
+            publicKeys: publicKey.keys,
+            format: 'binary'
           };
           return openpgp.sign(signOpt).then(async function (signed) {
             const message = await openpgp.message.read(signed.data);
@@ -2318,7 +2320,8 @@ describe('OpenPGP.js public api tests', function() {
           };
           const verifyOpt = {
             publicKeys: publicKey.keys,
-            streaming: 'web'
+            streaming: 'web',
+            format: 'binary'
           };
           return openpgp.sign(signOpt).then(async function (signed) {
             expect(openpgp.util.isStream(signed.data)).to.equal('web');
