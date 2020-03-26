1df9a37f8924b22cdbfbb14b3614c124afd8b541
larabr <7375870+larabr@users.noreply.github.com>
2020-03-26 17:57:10 +0100

    Use V5 keys by default (#1063)

--- src/config/config.js
+++ src/config/config.js
@@ -69,12 +69,11 @@ export default {
   aead_chunk_size_byte:     12,
   /**
    * Use V5 keys.
-   * **NOT INTEROPERABLE WITH OTHER OPENPGP IMPLEMENTATIONS**
-   * **FUTURE OPENPGP.JS VERSIONS MAY BREAK COMPATIBILITY WHEN USING THIS OPTION**
+   * Note: not all OpenPGP implementations are compatible with this option
    * @memberof module:config
    * @property {Boolean} v5_keys
    */
-  v5_keys:                  false,
+  v5_keys:                  true,
   /**
    * {@link https://tools.ietf.org/html/rfc4880#section-3.7.1.3|RFC4880 3.7.1.3}:
    * Iteration Count Byte for S2K (String to Key)
