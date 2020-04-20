openpgpjs/openpgpjs

commit 6119dbb08ed3df2d2d24a7baabc29378f9719a9f

Support verification of text signatures on non-UTF-8 messages (#1071)

src/util.js
@@ -689,27 +689,48 @@ export default {
   },
 
   /**
-   * Normalize line endings to \r\n
+   * Normalize line endings to <CR><LF>
+   * Support any encoding where CR=0x0D, LF=0x0A
    */
-  canonicalizeEOL: function(text) {
-    return stream.transform(util.nativeEOL(text), value => value.replace(/\r/g, "\n").replace(/\n/g, "\r\n"));
+  canonicalizeEOL: function(data) {
+    const CR = 13;
+    const LF = 10;
+
+    return stream.transform(util.nativeEOL(data, true), bytes => {
+      const normalized = [];
+      for (let i = 0; i < bytes.length; i++){
+        const x = bytes[i];
+        if (x === LF || x === CR) {
+          normalized.push(CR, LF);
+        } else {
+          normalized.push(x);
+        }
+      }
+      return new Uint8Array(normalized);
+    });
   },
 
   /**
-   * Convert line endings from canonicalized \r\n to native \n
+   * Convert line endings from canonicalized <CR><LF> to native <LF>
+   * Support any encoding where CR=0x0D, LF=0x0A
    */
-  nativeEOL: function(text) {
-    let lastChar = '';
-    return stream.transform(text, value => {
-      value = lastChar + value;
-      if (value[value.length - 1] === '\r') {
-        lastChar = '\r';
-        value = value.slice(0, -1);
+  nativeEOL: function(data) {
+    const CR = 13;
+    const LF = 10;
+    let carryOverCR = false;
+
+    return stream.transform(data, bytes => {
+      bytes = carryOverCR ? [CR].concat(Array.from(bytes)) : Array.from(bytes);
+
+      if (bytes[bytes.length - 1] === CR) {
+        carryOverCR = true;
+        bytes.pop();
       } else {
-        lastChar = '';
+        carryOverCR = false;
       }
-      return value.replace(/\r\n/g, '\n');
-    }, () => lastChar);
+
+      return new Uint8Array(bytes.filter((x, i, xs) => (x !== CR || (i < xs.length - 1 && xs[i + 1] !== LF))));
+    }, () => new Uint8Array(carryOverCR ? [CR] : []));
   },
 
   /**

src/cleartext.js
@@ -43,7 +43,7 @@ export function CleartextMessage(text, signature) {
     return new CleartextMessage(text, signature);
   }
   // normalize EOL to canonical form <CR><LF>
-  this.text = util.canonicalizeEOL(util.removeTrailingSpaces(text));
+  this.text = util.removeTrailingSpaces(text).replace(/\r\n/g, '\n').replace(/[\r\n]/g, '\r\n');
   if (signature && !(signature instanceof Signature)) {
     throw new Error('Invalid signature input');
   }
@@ -124,7 +124,7 @@ CleartextMessage.prototype.verifyDetached = function(signature, keys, date = new
  */
 CleartextMessage.prototype.getText = function() {
   // normalize end of line to \n
-  return util.nativeEOL(this.text);
+  return this.text.replace(/\r\n/g, '\n');
 };
 
 /**

src/packet/literal.js
@@ -64,7 +64,7 @@ Literal.prototype.setText = function(text, format = 'utf8') {
  */
 Literal.prototype.getText = function(clone = false) {
   if (this.text === null || util.isStream(this.text)) { // Assume that this.text has been read
-    this.text = util.nativeEOL(util.decode_utf8(this.getBytes(clone)));
+    this.text = util.decode_utf8(util.nativeEOL(this.getBytes(clone)));
   }
   return this.text;
 };
@@ -88,8 +88,8 @@ Literal.prototype.setBytes = function(bytes, format) {
  */
 Literal.prototype.getBytes = function(clone = false) {
   if (this.data === null) {
-    // normalize EOL to \r\n and encode UTF8
-    this.data = util.encode_utf8(util.canonicalizeEOL(this.text));
+    // encode UTF8 and normalize EOL to \r\n
+    this.data = util.canonicalizeEOL(util.encode_utf8(this.text));
   }
   if (clone) {
     return stream.passiveClone(this.data);

src/packet/signature.js
@@ -577,11 +577,9 @@ Signature.prototype.toSign = function (type, data) {
       return data.getBytes(true);
 
     case t.text: {
-      let text = data.getText(true);
+      const bytes = data.getBytes(true);
       // normalize EOL to \r\n
-      text = util.canonicalizeEOL(text);
-      // encode UTF8
-      return util.encode_utf8(text);
+      return util.canonicalizeEOL(bytes);
     }
     case t.standalone:
       return new Uint8Array(0);

commit c4a7455cb5820d4e36f00bc8c8dc399d5e9a4481

Fix memory usage when non-streaming-en/decrypting large files
Broken in #1071.

src/util.js
@@ -698,26 +698,41 @@ export default {
     let carryOverCR = false;
 
     return stream.transform(data, bytes => {
-      bytes = carryOverCR ? [CR].concat(Array.from(bytes)) : Array.from(bytes);
+      if (carryOverCR) {
+        bytes = util.concatUint8Array([new Uint8Array([CR]), bytes]);
+      }
 
       if (bytes[bytes.length - 1] === CR) {
         carryOverCR = true;
-        bytes.pop();
+        bytes = bytes.subarray(0, -1);
       } else {
         carryOverCR = false;
       }
 
-      const normalized = [];
-      for (let i = 0; i < bytes.length; i++){
-        const x = bytes[i];
-        if (x === LF && i > 0 && bytes[i - 1] !== CR) {
-          normalized.push(CR, LF);
-        } else {
-          normalized.push(x);
-        }
+      let index;
+      const indices = [];
+      for (let i = 0; ; i = index) {
+        index = bytes.indexOf(LF, i) + 1;
+        if (index && bytes[index - 2] !== CR) indices.push(index);
+        else break;
       }
-      return new Uint8Array(normalized);
-    }, () => new Uint8Array(carryOverCR ? [CR] : []));
+      if (!indices.length) {
+        return bytes;
+      }
+
+      const normalized = new Uint8Array(bytes.length + indices.length);
+      let j = 0;
+      for (let i = 0; i < indices.length; i++) {
+        const sub = bytes.subarray(indices[i - 1] || 0, indices[i]);
+        normalized.set(sub, j);
+        j += sub.length;
+        normalized[j - 1] = CR;
+        normalized[j] = LF;
+        j++;
+      }
+      normalized.set(bytes.subarray(indices[indices.length - 1] || 0), j);
+      return normalized;
+    }, () => (carryOverCR ? new Uint8Array([CR]) : undefined));
   },
 
   /**
@@ -730,17 +745,30 @@ export default {
     let carryOverCR = false;
 
     return stream.transform(data, bytes => {
-      bytes = carryOverCR ? [CR].concat(Array.from(bytes)) : Array.from(bytes);
+      if (carryOverCR && bytes[0] !== LF) {
+        bytes = util.concatUint8Array([new Uint8Array([CR]), bytes]);
+      } else {
+        bytes = new Uint8Array(bytes); // Don't mutate passed bytes
+      }
 
       if (bytes[bytes.length - 1] === CR) {
         carryOverCR = true;
-        bytes.pop();
+        bytes = bytes.subarray(0, -1);
       } else {
         carryOverCR = false;
       }
 
-      return new Uint8Array(bytes.filter((x, i, xs) => (x !== CR || (i < xs.length - 1 && xs[i + 1] !== LF))));
-    }, () => new Uint8Array(carryOverCR ? [CR] : []));
+      let index;
+      let j = 0;
+      for (let i = 0; i !== bytes.length; i = index) {
+        index = bytes.indexOf(CR, i) + 1;
+        if (!index) index = bytes.length;
+        const last = index - (bytes[index] === LF ? 1 : 0);
+        if (i) bytes.copyWithin(j, i, last);
+        j += last - i;
+      }
+      return bytes.subarray(0, j);
+    }, () => (carryOverCR ? new Uint8Array([CR]) : undefined));
   },
 
   /**