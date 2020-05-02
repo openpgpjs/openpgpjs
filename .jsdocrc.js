18b0121889d4989eea398bf82f79ee709905c751
Daniel Huigens <d.huigens@protonmail.com>
2020-05-02 02:42:39 +0200

    Link to source code on GitHub from documentation
    Instead of including the entire source code in the documentation.

--- .jsdocrc.js
+++ .jsdocrc.js
@@ -1,6 +1,16 @@
+const pkg = require('./package.json');
+
 module.exports = {
     plugins: ['plugins/markdown'],
     markdown: {
         idInHeadings: true
+    },
+    templates: {
+        default: {
+            outputSourceFiles: false,
+            externalSourceLinks: {
+                urlPrefix: `${pkg.repository.url}/blob/v${pkg.version}/src/`
+            }
+        }
     }
 };
