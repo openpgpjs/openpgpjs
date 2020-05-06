857c744430c869f1c684e1c9d12d364981478fc2
Ilya Chesnokov <ilya.chesnokov@protonmail.com>
2020-05-06 16:46:04 +0700

    move apt install to travis.yml

--- run-sop.sh
+++ run-sop.sh
@@ -1,5 +1,4 @@
 cd ../openpgp-interoperability-test-suite-binary
-sudo apt install -y nettle-dev
 touch config.json
 echo '{"drivers": [{"driver": "sop", "path": "../sop-openpgp-js/sop-openpgp"}]}' > config.json
 ./target/release/openpgp-interoperability-test-suite > $TRAVIS_BUILD_DIR/report.html
