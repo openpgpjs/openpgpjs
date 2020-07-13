0806eb6672e91ac142eaf51bbb3a00d3b1c1cb10
Dan Ristea <danraduristea@gmail.com>
2020-07-13 19:18:26 +0100

    Fix comments on symmetric- and public-key encrypted session keys (#1120)

--- src/packet/public_key_encrypted_session_key.js
+++ src/packet/public_key_encrypted_session_key.js
@@ -63,11 +63,7 @@ class PublicKeyEncryptedSessionKeyPacket {
   /**
    * Parsing function for a publickey encrypted session key packet (tag 1).
    *
-   * @param {Uint8Array} input Payload of a tag 1 packet
-   * @param {Integer} position Position to start reading from the input string
-   * @param {Integer} len Length of the packet or the remaining length of
-   *            input at position
-   * @returns {PublicKeyEncryptedSessionKeyPacket} Object representation
+   * @param {Uint8Array} bytes Payload of a tag 1 packet
    */
   read(bytes) {
     this.version = bytes[0];
@@ -86,7 +82,7 @@ class PublicKeyEncryptedSessionKeyPacket {
   }
 
   /**
-   * Create a string representation of a tag 1 packet
+   * Create a binary representation of a tag 1 packet
    *
    * @returns {Uint8Array} The Uint8Array representation
    */
