eb15e9c00be606424dcfb1ec34a6ba40c85537c5
Tom J <tom@holub.me>
2020-12-09 21:23:07 +0000

    comment, clean up

--- test/typescript/definitions.ts
+++ test/typescript/definitions.ts
@@ -1,14 +1,10 @@
 /**
- * Testing typescript definitions.
- *
+ * npm run-script test-type-definitions
+ * 
  * If types are off, either this will fail to build with TypeScript, or it will fail to run.
  *  - if it fails to build, edit the file to match type definitions
  *  - if it failt to run, edit the definitions (and consequently this file) to match actual library API
- *
- * the triple /// below brings in the type definitions manually
  */
-
-/// - <reference path="../../openpgp.d.ts" />
 
 import { generateKey, readArmoredKey, readArmoredKeys, Key, Message, encrypt } from '../../dist/node/openpgp';
 import { expect } from 'chai';
