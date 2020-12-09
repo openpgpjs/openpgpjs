81a7964dee54bcb476b981d54553796602f6c281
Tom J <tom@holub.me>
2020-12-09 20:35:49 +0000

    test attempt

---
+++ test/typescript/definitions.ts
@@ -0,0 +1,14 @@
+/**
+ * Testing typescript definitions.
+ *
+ * If types are off, either this will fail to build with TypeScript, or it will fail to run.
+ *  - if it fails to build, edit the file to match type definitions
+ *  - if it failt to run, edit the definitions (and consequently this file) to match actual library API
+ *
+ * the triple /// below brings in the type definitions manually
+ */
+
+/// - <reference path="../../openpgp.d.ts" />
+
+import { } from '../../dist/openpgp';
+
