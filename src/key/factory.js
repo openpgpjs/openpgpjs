01f40af02887cccdfe9c1aac898da289ee1b2aea
larabr <7375870+larabr@users.noreply.github.com>
2021-03-01 18:41:29 +0100

    Do not support creating sym. enc. messages without integrity protection

--- src/key/factory.js
+++ src/key/factory.js
@@ -181,10 +181,9 @@ async function wrapKeyObject(secretKeyPacket, secretSubkeyPackets, options, conf
     if (index === 0) {
       signaturePacket.isPrimaryUserID = true;
     }
-    if (config.integrityProtect) {
-      signaturePacket.features = [0];
-      signaturePacket.features[0] |= enums.features.modificationDetection;
-    }
+    // integrity protection always enabled
+    signaturePacket.features = [0];
+    signaturePacket.features[0] |= enums.features.modificationDetection;
     if (config.aeadProtect) {
       signaturePacket.features || (signaturePacket.features = [0]);
       signaturePacket.features[0] |= enums.features.aead;
