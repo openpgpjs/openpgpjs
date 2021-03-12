3ccf74a68f43903948b2e422062c7c165843c1a3
larabr <7375870+larabr@users.noreply.github.com>
2021-03-12 18:11:00 +0100

    Fix tests

--- test/general/ecc_nist.js
+++ test/general/ecc_nist.js
@@ -3,63 +3,49 @@ const openpgp = typeof window !== 'undefined' && window.openpgp ? window.openpgp
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
 const input = require('./testInputs.js');
+const util = require('../../src/util');
 
 const expect = chai.expect;
 
 module.exports = () => describe('Elliptic Curve Cryptography for NIST P-256,P-384,P-521 curves @lightweight', function () {
   function omnibus() {
-    it('Omnibus NIST P-256 Test', function () {
-      const options = { userIds: { name: "Hi", email: "hi@hel.lo" }, curve: "p256" };
+    it('Omnibus NIST P-256 Test', async function () {
       const testData = input.createSomeMessage();
       const testData2 = input.createSomeMessage();
-      return openpgp.generateKey(options).then(function (firstKey) {
-        const hi = firstKey.key;
-        const pubHi = hi.toPublic();
 
-        const options = { userIds: { name: "Bye", email: "bye@good.bye" }, curve: "p256" };
-        return openpgp.generateKey(options).then(function (secondKey) {
-          const bye = secondKey.key;
-          const pubBye = bye.toPublic();
+      const firstKey = await openpgp.generateKey({ userIds: { name: "Hi", email: "hi@hel.lo" }, curve: "p256" });
+      const hi = firstKey.key;
+      const pubHi = hi.toPublic();
+      const secondKey = await openpgp.generateKey({ userIds: { name: "Bye", email: "bye@good.bye" }, curve: "p256" });
+      const bye = secondKey.key;
+      const pubBye = bye.toPublic();
 
-          return Promise.all([
-            // Signing message
+      const cleartextMessage = await openpgp.sign({ message: openpgp.CleartextMessage.fromText(testData), privateKeys: hi });
+      await openpgp.verify({
+        message: await openpgp.readCleartextMessage({ cleartextMessage }),
+        publicKeys: pubHi
+      }).then(output => expect(output.signatures[0].valid).to.be.true);
+      // Verifying detached signature
+      await openpgp.verify({
+        message: openpgp.Message.fromText(util.removeTrailingSpaces(testData)),
+        publicKeys: pubHi,
+        signature: (await openpgp.readCleartextMessage({ cleartextMessage })).signature
+      }).then(output => expect(output.signatures[0].valid).to.be.true);
 
-            openpgp.sign(
-              { message: openpgp.CleartextMessage.fromText(testData), privateKeys: hi }
-            ).then(async signed => {
-              const msg = await openpgp.readCleartextMessage({ cleartextMessage: signed });
-              // Verifying signed message
-              return Promise.all([
-                openpgp.verify(
-                  { message: msg, publicKeys: pubHi }
-                ).then(output => expect(output.signatures[0].valid).to.be.true),
-                // Verifying detached signature
-                openpgp.verify({
-                  message: openpgp.CleartextMessage.fromText(testData),
-                  publicKeys: pubHi,
-                  signature: msg.signature
-                }).then(output => expect(output.signatures[0].valid).to.be.true)
-              ]);
-            }),
-            // Encrypting and signing
-            openpgp.encrypt(
-              { message: openpgp.Message.fromText(testData2),
-                publicKeys: [pubBye],
-                privateKeys: [hi] }
-            ).then(async encrypted => {
-              const msg = await openpgp.readMessage({ armoredMessage: encrypted });
-              // Decrypting and verifying
-              return openpgp.decrypt(
-                { message: msg,
-                  privateKeys: bye,
-                  publicKeys: [pubHi] }
-              ).then(output => {
-                expect(output.data).to.equal(testData2);
-                expect(output.signatures[0].valid).to.be.true;
-              });
-            })
-          ]);
-        });
+      // Encrypting and signing
+      const encrypted = await openpgp.encrypt({
+        message: openpgp.Message.fromText(testData2),
+        publicKeys: [pubBye],
+        privateKeys: [hi]
+      });
+      // Decrypting and verifying
+      return openpgp.decrypt({
+        message: await openpgp.readMessage({ armoredMessage: encrypted }),
+        privateKeys: bye,
+        publicKeys: [pubHi]
+      }).then(output => {
+        expect(output.data).to.equal(testData2);
+        expect(output.signatures[0].valid).to.be.true;
       });
     });
   }
