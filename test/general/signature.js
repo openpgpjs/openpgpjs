98756fbcc43e5fab47ba2d0fe912f48d2b4002a3
Dan Habot <dan.habot@gmail.com>
2021-06-14 11:59:14 -0400

    Update test/general/signature.js
    Co-authored-by: Daniel Huigens <d.huigens@protonmail.com>

--- test/general/signature.js
+++ test/general/signature.js
@@ -692,7 +692,6 @@ kCNcH9WI6idSzFjuYegECf+ZA1xOCjS9oLTGbSeT7jNfC8dH5+E92qlBLq4Ctt7k
     await expect(openpgp.readSignature({ armoredSignature })).to.be.rejectedWith(/Missing signature creation time/);
   });
 
-
   it('Testing signature checking on CAST5-enciphered message', async function() {
     const publicKey = await openpgp.readKey({ armoredKey: pub_key_arm1 });
     const privateKey = await openpgp.decryptKey({
