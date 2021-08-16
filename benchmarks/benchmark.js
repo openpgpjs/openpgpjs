4c06461417dff66f463146ccdd24fe04587a8e83
larabr <7375870+larabr@users.noreply.github.com>
2021-08-16 18:30:54 +0200

    Test benchmark action

---
+++ benchmarks/benchmark.js
@@ -0,0 +1,25 @@
+const Benchmark = require('benchmark');
+const openpgp = typeof window !== 'undefined' && window.openpgp ? window.openpgp : require('../');
+
+const wrapAsync = func => ({
+  fn: async deferred => {
+    await func();
+    deferred.resolve();
+  },
+  defer: true
+});
+
+function runBenchmarks() {
+  const suite = new Benchmark.Suite();
+
+  suite.add('some test case', wrapAsync(async () => {
+    await openpgp.generateKey({ userIDs: { email: 'test@test.it' } });
+  }));
+
+  suite.on('cycle', event => {
+    // Output benchmark result by converting benchmark result to string
+    console.log(String(event.target));
+  }).run({ 'async': true });
+}
+
+runBenchmarks();
