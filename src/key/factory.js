60e126f93c1ea978d70bc04cb45c9b63727aa6ed
Daniel Huigens <d.huigens@protonmail.com>
2022-03-02 17:18:44 +0100

    Generate SEIPD V2 flag instead of AEAD flag
    The AEAD Encrypted Data packet has been removed from the draft
    in favor of version 2 of the Sym. Encrypted Integrity Protected
    Data packet. It also has a new feature flag to match.

--- src/key/factory.js
+++ src/key/factory.js
@@ -201,7 +201,7 @@ async function wrapKeyObject(secretKeyPacket, secretSubkeyPackets, options, conf
     signaturePacket.features = [0];
     signaturePacket.features[0] |= enums.features.modificationDetection;
     if (config.aeadProtect) {
-      signaturePacket.features[0] |= enums.features.aead;
+      signaturePacket.features[0] |= enums.features.seipdv2;
     }
     if (config.v5Keys) {
       signaturePacket.features[0] |= enums.features.v5Keys;
