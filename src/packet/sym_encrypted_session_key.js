e5fe84dc2e81010780ae45061e92d20f456c0fe6
Daniel Huigens <d.huigens@protonmail.com>
2022-12-14 18:08:01 +0100

    Support SKESK v6 as per the latest crypto refresh
    The latest crypto refresh specifies an HKDF step to be used for
    deriving the key to encrypt the session key with.
    
    It also specifies two additional length fields.

--- src/packet/sym_encrypted_session_key.js
+++ src/packet/sym_encrypted_session_key.js
@@ -18,6 +18,7 @@
 import { newS2KFromConfig, newS2KFromType } from '../type/s2k';
 import defaultConfig from '../config';
 import crypto from '../crypto';
+import computeHKDF from '../crypto/hkdf';
 import enums from '../enums';
 import util from '../util';
 import { UnsupportedError } from './packet';
@@ -44,7 +45,7 @@ class SymEncryptedSessionKeyPacket {
    * @param {Object} [config] - Full configuration, defaults to openpgp.config
    */
   constructor(config = defaultConfig) {
-    this.version = config.aeadProtect ? 5 : 4;
+    this.version = config.aeadProtect ? 6 : 4;
     this.sessionKey = null;
     /**
      * Algorithm to encrypt the session key with
@@ -74,18 +75,28 @@ class SymEncryptedSessionKeyPacket {
   read(bytes) {
     let offset = 0;
 
-    // A one-octet version number. The only currently defined version is 4.
+    // A one-octet version number with value 4, 5 or 6.
     this.version = bytes[offset++];
-    if (this.version !== 4 && this.version !== 5) {
+    if (this.version !== 4 && this.version !== 5 && this.version !== 6) {
       throw new UnsupportedError(`Version ${this.version} of the SKESK packet is unsupported.`);
+    }
+
+    if (this.version === 6) {
+      // A one-octet scalar octet count of the following 5 fields.
+      offset++;
     }
 
     // A one-octet number describing the symmetric algorithm used.
     const algo = bytes[offset++];
 
-    if (this.version === 5) {
+    if (this.version >= 5) {
       // A one-octet AEAD algorithm.
       this.aeadAlgorithm = bytes[offset++];
+
+      if (this.version === 6) {
+        // A one-octet scalar octet count of the following field.
+        offset++;
+      }
     }
 
     // A string-to-key (S2K) specifier, length as defined above.
@@ -93,7 +104,7 @@ class SymEncryptedSessionKeyPacket {
     this.s2k = newS2KFromType(s2kType);
     offset += this.s2k.read(bytes.subarray(offset, bytes.length));
 
-    if (this.version === 5) {
+    if (this.version >= 5) {
       const mode = crypto.getAEADMode(this.aeadAlgorithm);
 
       // A starting initialization vector of size specified by the AEAD
@@ -103,7 +114,7 @@ class SymEncryptedSessionKeyPacket {
 
     // The encrypted session key itself, which is decrypted with the
     // string-to-key object. This is optional in version 4.
-    if (this.version === 5 || offset < bytes.length) {
+    if (this.version >= 5 || offset < bytes.length) {
       this.encrypted = bytes.subarray(offset, bytes.length);
       this.sessionKeyEncryptionAlgorithm = algo;
     } else {
@@ -123,10 +134,15 @@ class SymEncryptedSessionKeyPacket {
 
     let bytes;
 
-    if (this.version === 5) {
-      bytes = util.concatUint8Array([new Uint8Array([this.version, algo, this.aeadAlgorithm]), this.s2k.write(), this.iv, this.encrypted]);
+    const s2k = this.s2k.write();
+    if (this.version === 6) {
+      const s2kLen = s2k.length;
+      const fieldsLen = 3 + s2kLen + this.iv.length;
+      bytes = util.concatUint8Array([new Uint8Array([this.version, fieldsLen, algo, this.aeadAlgorithm, s2kLen]), s2k, this.iv, this.encrypted]);
+    } else if (this.version === 5) {
+      bytes = util.concatUint8Array([new Uint8Array([this.version, algo, this.aeadAlgorithm]), s2k, this.iv, this.encrypted]);
     } else {
-      bytes = util.concatUint8Array([new Uint8Array([this.version, algo]), this.s2k.write()]);
+      bytes = util.concatUint8Array([new Uint8Array([this.version, algo]), s2k]);
 
       if (this.encrypted !== null) {
         bytes = util.concatUint8Array([bytes, this.encrypted]);
@@ -150,10 +166,11 @@ class SymEncryptedSessionKeyPacket {
     const { blockSize, keySize } = crypto.getCipher(algo);
     const key = await this.s2k.produceKey(passphrase, keySize);
 
-    if (this.version === 5) {
+    if (this.version >= 5) {
       const mode = crypto.getAEADMode(this.aeadAlgorithm);
       const adata = new Uint8Array([0xC0 | SymEncryptedSessionKeyPacket.tag, this.version, this.sessionKeyEncryptionAlgorithm, this.aeadAlgorithm]);
-      const modeInstance = await mode(algo, key);
+      const encryptionKey = this.version === 6 ? await computeHKDF(enums.hash.sha256, key, new Uint8Array(), adata, keySize) : key;
+      const modeInstance = await mode(algo, encryptionKey);
       this.sessionKey = await modeInstance.decrypt(this.encrypted, this.iv, adata);
     } else if (this.encrypted !== null) {
       const decrypted = await crypto.mode.cfb.decrypt(algo, key, this.encrypted, new Uint8Array(blockSize));
@@ -183,24 +200,25 @@ class SymEncryptedSessionKeyPacket {
     this.s2k.generateSalt();
 
     const { blockSize, keySize } = crypto.getCipher(algo);
-    const encryptionKey = await this.s2k.produceKey(passphrase, keySize);
+    const key = await this.s2k.produceKey(passphrase, keySize);
 
     if (this.sessionKey === null) {
       this.sessionKey = crypto.generateSessionKey(this.sessionKeyAlgorithm);
     }
 
-    if (this.version === 5) {
+    if (this.version >= 5) {
       const mode = crypto.getAEADMode(this.aeadAlgorithm);
       this.iv = crypto.random.getRandomBytes(mode.ivLength); // generate new random IV
-      const associatedData = new Uint8Array([0xC0 | SymEncryptedSessionKeyPacket.tag, this.version, this.sessionKeyEncryptionAlgorithm, this.aeadAlgorithm]);
+      const adata = new Uint8Array([0xC0 | SymEncryptedSessionKeyPacket.tag, this.version, this.sessionKeyEncryptionAlgorithm, this.aeadAlgorithm]);
+      const encryptionKey = this.version === 6 ? await computeHKDF(enums.hash.sha256, key, new Uint8Array(), adata, keySize) : key;
       const modeInstance = await mode(algo, encryptionKey);
-      this.encrypted = await modeInstance.encrypt(this.sessionKey, this.iv, associatedData);
+      this.encrypted = await modeInstance.encrypt(this.sessionKey, this.iv, adata);
     } else {
       const toEncrypt = util.concatUint8Array([
         new Uint8Array([this.sessionKeyAlgorithm]),
         this.sessionKey
       ]);
-      this.encrypted = await crypto.mode.cfb.encrypt(algo, encryptionKey, toEncrypt, new Uint8Array(blockSize), config);
+      this.encrypted = await crypto.mode.cfb.encrypt(algo, key, toEncrypt, new Uint8Array(blockSize), config);
     }
   }
 }
