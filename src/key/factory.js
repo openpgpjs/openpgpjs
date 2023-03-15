87d2f9cb3b4afbade8f408e519eeade2c6b979a3
Daniel Huigens <d.huigens@protonmail.com>
2023-03-15 19:37:55 +0100

    Replace config.v5Keys with config.v6Keys flag
    Also, don't generate v5 keys flag, which has been removed from the draft specification.

--- src/key/factory.js
+++ src/key/factory.js
@@ -233,9 +233,6 @@ async function wrapKeyObject(secretKeyPacket, secretSubkeyPackets, options, conf
     if (config.aeadProtect) {
       signaturePacket.features[0] |= enums.features.aead;
     }
-    if (config.v5Keys) {
-      signaturePacket.features[0] |= enums.features.v5Keys;
-    }
     if (options.keyExpirationTime > 0) {
       signaturePacket.keyExpirationTime = options.keyExpirationTime;
       signaturePacket.keyNeverExpires = false;
