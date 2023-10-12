4ee9deae626f2bdd5b005a7848d7408b69114ec4
larabr <7375870+larabr@users.noreply.github.com>
2023-10-12 11:16:55 +0200

    Switch back to using standard BigInteger class instead of wrapper
    Using a wrapper requires adding some handling code to fix race conditions,
    but it does not provide advantages until we switch to TS.

--- src/biginteger.js
+++ src/biginteger.js
@@ -1,39 +1,20 @@
 /**
- * This is a vanilla JS copy of @openpgp/noble-hashes/esm/biginteger/interface.ts .
- * We need to duplicate the file, instead of importing it, since in that case the BigIntegerInterface instance
- * would be shared with noble-hashes, which separately calls `setImplementation()` on load, causing it to throw due to
- * duplicate initialization.
+ * We don't use the BigIntegerInterface wrapper from noble-hashes because:
+ * - importing the instance results in it being shared with noble-hashes, which separately calls `setImplementation()`
+ *  on load, causing it to throw due to duplicate initialization.
+ * - even duplicating the interface code here to keep a separate instance requires handing a race-conditions the first time
+ * `getBigInteger` is called, when the code needs to check if the implementation is set, and initialize it if not.
+ * Ultimately, the interface provides no advantages and it's only needed because of TS.
  */
-class BigInteger {
-  static setImplementation(Implementation, replace = false) {
-    if (BigInteger.Implementation && !replace) {
-      throw new Error('Implementation already set');
-    }
-    BigInteger.Implementation = Implementation;
-  }
-
-  static new(n) {
-    return new BigInteger.Implementation(n);
-  }
-}
-
 const detectBigInt = () => typeof BigInt !== 'undefined';
 export async function getBigInteger() {
-  if (BigInteger.Implementation) {
-    return BigInteger;
-  }
-
-  // TODOOOOO replace = true needed in case of concurrent class loading, how to fix without removing wrapper class?
-
   if (detectBigInt()) {
     // NativeBigInteger is small, so it's imported in isolation (it could also be imported at the top level)
     const { default: NativeBigInteger } = await import('@openpgp/noble-hashes/esm/biginteger/native.interface');
-    BigInteger.setImplementation(NativeBigInteger, true);
+    return NativeBigInteger;
   } else {
     // FallbackBigInteger relies on large BN.js lib, which is also used by noble-hashes and noble-curves
     const { default: FallbackBigInteger } = await import('@openpgp/noble-hashes/esm/biginteger/bn.interface');
-    BigInteger.setImplementation(FallbackBigInteger, true);
+    return FallbackBigInteger;
   }
-
-  return BigInteger;
 }
