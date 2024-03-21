aba61efa702eef0c6800bec5eccdea2c67d13035
larabr <7375870+larabr@users.noreply.github.com>
2024-03-21 14:09:23 +0100

    CI: update interop test suite: fix sop-openpgpjs, include crypto-refresh tests, and compare with gopenpgp v3
    sop-openpgpjs did not correctly apply the `OPENPGPJS_PATH` env variable; as a result,
    it did not actually test the code from either the PR and base branch, but always from
    the hardcoded version bundled with it.

--- .github/test-suite/prepare_config.sh
+++ .github/test-suite/prepare_config.sh
@@ -7,7 +7,7 @@ cat $CONFIG_TEMPLATE \
     | sed "s@__OPENPGPJS_MAIN__@${OPENPGPJS_MAIN}@g" \
     | sed "s@__SQOP__@${SQOP}@g" \
     | sed "s@__GPGME_SOP__@${GPGME_SOP}@g" \
-    | sed "s@__GOSOP__@${GOSOP}@g" \
+    | sed "s@__GOSOP_V2__@${GOSOP_DIR_V2}/gosop@g" \
     | sed "s@__SOP_OPENPGPJS__@${SOP_OPENPGPJS}@g" \
     | sed "s@__RNP_SOP__@${RNP_SOP}@g" \
     > $CONFIG_OUTPUT
