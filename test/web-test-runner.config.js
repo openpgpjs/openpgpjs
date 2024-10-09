4ddadd4f539490ec480dd06f01d39ae0fae1ccb7
larabr <7375870+larabr@users.noreply.github.com>
2024-10-09 17:20:16 +0200

    CI: setup HTTPS in web-test-runner for BrowserStack tests
    To have tests work Browserstack Safari (also below iOS 15), as the tests are run in an iframe,
    rewriting localhost as hostname, making WebCrypto not available.
    
    We keep HTTP for the non-browserstack tests so that in local testing,
    generating self-signed certs is not required.

--- test/web-test-runner.config.js
+++ test/web-test-runner.config.js
@@ -1,50 +1,33 @@
-import { browserstackLauncher } from '@web/test-runner-browserstack';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
-const sharedBrowserstackCapabilities = {
-  'browserstack.user': process.env.BROWSERSTACK_USERNAME,
-  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
-
-  project: `openpgpjs/${process.env.GITHUB_EVENT_NAME || 'push'}${process.env.LIGHTWEIGHT ? '/lightweight' : ''}`,
-  name: process.env.GITHUB_WORKFLOW,
-  build: process.env.GITHUB_SHA,
-  timeout: 450
+const sharedPlaywrightCIOptions = {
+  // createBrowserContext: ({ browser }) => browser.newContext({ ignoreHTTPSErrors: true }),
+  headless: true
 };
 
 export default {
   nodeResolve: true, // to resolve npm module imports in `unittests.html`
   files: './test/unittests.html',
-
+  protocol: 'http:',
+  hostname: '127.0.0.1',
+  testsStartTimeout: 45000,
+  browserStartTimeout: 120000,
+  testsFinishTimeout: 450000,
+  concurrentBrowsers: 3,
+  concurrency: 1, // see https://github.com/modernweb-dev/web/issues/2706
+  coverage: false,
   groups: [
     { name: 'local' }, // group meant to be used with either --browser or --manual options via CLI
     {
-      name: 'browserstack',
-      browsers: process.env.BROWSERSTACK_USERNAME && [
-        browserstackLauncher({
-          capabilities: {
-            ...sharedBrowserstackCapabilities,
-            browserName: 'Safari',
-            browser_version: 'latest', // Webkit and Safari can differ in behavior
-            os: 'OS X',
-            os_version: 'Ventura'
-          }
+      name: 'headless:ci',
+      browsers: [
+        playwrightLauncher({
+          ...sharedPlaywrightCIOptions,
+          product: 'chromium'
         }),
-        browserstackLauncher({
-          capabilities: {
-            ...sharedBrowserstackCapabilities,
-            browserName: 'Safari',
-            browser_version: '14', // min supported version
-            os: 'OS X',
-            os_version: 'Big Sur'
-          }
-        }),
-        browserstackLauncher({
-          capabilities: {
-            ...sharedBrowserstackCapabilities,
-            device: 'iPhone 12',
-            real_mobile: true,
-            os: 'ios',
-            os_version: '14'
-          }
+        playwrightLauncher({
+          ...sharedPlaywrightCIOptions,
+          product: 'firefox'
         })
       ]
     }
