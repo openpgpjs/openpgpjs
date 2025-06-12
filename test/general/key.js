5afbf72c304a786732397b747432a14566be9a4e
martgil <46025304+martgil@users.noreply.github.com>
2025-06-12 11:03:36 +0800

    test: revert changes - follow RFC 1123

--- test/general/key.js
+++ test/general/key.js
@@ -2399,13 +2399,13 @@ function versionSpecificTests() {
   });
 
   it('Generate key - single userID (special email format)', async function() {
-    const userID = { name: 'test', email: 'test1@example.com', comment: '' };
+    const userID = { name: 'test', email: 'test1@com.com09', comment: '' };
     const opt = { userIDs: userID };
     const { privateKey: armoredKey } = await openpgp.generateKey(opt);
     // test also serialisation and parsing
     const key = await openpgp.readKey({ armoredKey });
     expect(key.users.length).to.equal(1);
-    expect(key.users[0].userID.userID).to.equal('test <test1@example.com>');
+    expect(key.users[0].userID.userID).to.equal('test <test1@com.com09>');
     expect(key.users[0].userID.name).to.equal(userID.name);
     expect(key.users[0].userID.email).to.equal(userID.email);
     expect(key.users[0].userID.comment).to.equal(userID.comment);
