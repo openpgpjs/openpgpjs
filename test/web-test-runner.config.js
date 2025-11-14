04c59a297a700f27bd0cd79d9f4b86aef3dda278
larabr <7375870+larabr@users.noreply.github.com>
2025-11-14 12:45:42 +0100

    CI: increase browser timeout (all browsers)
    4aad1d1e34dd9cb6e5846364c0de549d27cc6e64 was only targeting
    Browserstack (by mistake). This applies the change to all browsers.

--- test/web-test-runner.config.js
+++ test/web-test-runner.config.js
@@ -12,7 +12,7 @@ export default {
   hostname: '127.0.0.1',
   testsStartTimeout: 45000,
   browserStartTimeout: 120000,
-  testsFinishTimeout: 450000,
+  testsFinishTimeout: 600000,
   concurrentBrowsers: 3,
   concurrency: 1, // see https://github.com/modernweb-dev/web/issues/2706
   coverage: false,
