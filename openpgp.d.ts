openpgpjs/openpgpjs

commit 48d1cfd15f3cfe37c2bcf0666060c0ec460b57a6

Linter: enable type info integration for rules (`recommended-type-checked`)

openpgp.d.ts
@@ -337,10 +337,9 @@ export class Message<T extends MaybeStream<Data>> {
 }
 
 /* ############## PACKET #################### */
-
-export declare abstract class BasePacket {
+export declare abstract class BasePacket<AsyncRead extends boolean = false> {
   static readonly tag: enums.packet;
-  public read(bytes: Uint8Array): void;
+  public read(bytes: Uint8Array): AsyncRead extends true ? Promise<void> : void;
   public write(): Uint8Array;
 }
 
@@ -349,7 +348,7 @@ export declare abstract class BasePacket {
  * - A Secret (Sub)Key Packet can always be used when a Public one is expected.
  * - A Subkey Packet cannot always be used when a Primary Key Packet is expected (and vice versa).
  */
-declare abstract class BasePublicKeyPacket extends BasePacket {
+declare abstract class BasePublicKeyPacket extends BasePacket<true> {
   public algorithm: enums.publicKey;
   public created: Date;
   public version: number;
@@ -397,20 +396,20 @@ export class SecretSubkeyPacket extends BaseSecretKeyPacket {
   protected isSubkey(): true;
 }
 
-export class CompressedDataPacket extends BasePacket {
+export class CompressedDataPacket extends BasePacket<true> {
   static readonly tag: enums.packet.compressedData;
   private compress(): void;
   private decompress(config?: Config): void;
 }
 
-export class SymEncryptedIntegrityProtectedDataPacket extends BasePacket {
+export class SymEncryptedIntegrityProtectedDataPacket extends BasePacket<true> {
   static readonly tag: enums.packet.symEncryptedIntegrityProtectedData;
 }
 
-export class AEADEncryptedDataPacket extends BasePacket {
+export class AEADEncryptedDataPacket extends BasePacket<true> {
   static readonly tag: enums.packet.aeadEncryptedData;
-  private decrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): void;
-  private encrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): void;
+  private decrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): Promise<void>;
+  private encrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): Promise<void>;
   private crypt(fn: (block: Uint8Array) => Uint8Array, sessionKey: Uint8Array, data: MaybeStream<Uint8Array>): MaybeStream<Uint8Array>;
 }
 
@@ -426,7 +425,7 @@ export class SymEncryptedSessionKeyPacket extends BasePacket {
   private encrypt(passphrase: string, config?: Config): Promise<void>;
 }
 
-export class LiteralDataPacket extends BasePacket {
+export class LiteralDataPacket extends BasePacket<true> {
   static readonly tag: enums.packet.literalData;
   private getText(clone?: boolean): MaybeStream<string>;
   private getBytes(clone?: boolean): MaybeStream<Uint8Array>;
@@ -439,8 +438,8 @@ export class LiteralDataPacket extends BasePacket {
 
 export class SymmetricallyEncryptedDataPacket extends BasePacket {
   static readonly tag: enums.packet.symmetricallyEncryptedData;
-  private decrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): void;
-  private encrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): void;
+  private decrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): Promise<void>;
+  private encrypt(sessionKeyAlgorithm: enums.symmetric, sessionKey: Uint8Array, config?: Config): Promise<void>;
 }
 
 export class MarkerPacket extends BasePacket {
@@ -547,8 +546,8 @@ export type AnyKeyPacket = BasePublicKeyPacket;
 
 type AllowedPackets = Map<enums.packet, object>; // mapping to Packet classes (i.e. typeof LiteralDataPacket etc.)
 export class PacketList<T extends AnyPacket> extends Array<T> {
-  static fromBinary(bytes: MaybeStream<Uint8Array>, allowedPackets: AllowedPackets, config?: Config): PacketList<AnyPacket>; // the packet types depend on`allowedPackets`
-  public read(bytes: MaybeStream<Uint8Array>, allowedPackets: AllowedPackets, config?: Config): void;
+  static fromBinary(bytes: MaybeStream<Uint8Array>, allowedPackets: AllowedPackets, config?: Config): Promise<PacketList<AnyPacket>>; // the packet types depend on`allowedPackets`
+  public read(bytes: MaybeStream<Uint8Array>, allowedPackets: AllowedPackets, config?: Config): Promise<void>;
   public write(): Uint8Array;
   public filterByTag(...args: enums.packet[]): PacketList<T>;
   public indexOfTag(...tags: enums.packet[]): number[];

test/typescript/definitions.ts
@@ -218,10 +218,12 @@ import {
   try {
     const nodeTextStream = NodeNativeReadableStream.toWeb(createReadStream('non-existent-file', { encoding: 'utf8' }));
     const messageFromNodeTextStream = await createMessage({ text: nodeTextStream });
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     (await encrypt({ message: messageFromNodeTextStream, passwords: 'password', format: 'armored' })) as NodeWebStream<string>;
   } catch {}
   const webTextStream = new WebReadableStream<string>();
   const messageFromWebTextStream = await createMessage({ text: webTextStream });
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   (await encrypt({ message: messageFromWebTextStream, passwords: 'password', format: 'armored' })) as WebStream<string>;
   messageFromWebTextStream.getText() as WebStream<string>;
   messageFromWebTextStream.getLiteralData() as WebStream<Uint8Array>;
@@ -230,10 +232,12 @@ import {
   try {
     const nodeBinaryStream = NodeNativeReadableStream.toWeb(createReadStream('non-existent-file'));
     const messageFromNodeBinaryStream = await createMessage({ binary: nodeBinaryStream });
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     (await encrypt({ message: messageFromNodeBinaryStream, passwords: 'password', format: 'binary' })) as NodeWebStream<Uint8Array>;
   } catch {}
   const webBinaryStream = new WebReadableStream<Uint8Array>();
   const messageFromWebBinaryStream = await createMessage({ binary: webBinaryStream });
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   (await encrypt({ message: messageFromWebBinaryStream, passwords: 'password', format: 'binary' })) as WebStream<Uint8Array>;
   messageFromWebBinaryStream.getText() as WebStream<string>;
   messageFromWebBinaryStream.getLiteralData() as WebStream<Uint8Array>;

eslint.config.js
@@ -12,7 +12,7 @@ import pluginUnicorn from 'eslint-plugin-unicorn';
 
 export default defineConfig(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
   globalIgnores(['dist/', 'test/lib/', 'docs/', '.jsdocrc.cjs']),
   {
     languageOptions: {
@@ -89,7 +89,7 @@ export default defineConfig(
       'prefer-spread': 'off', // TODO get rid of this
       'prefer-template': 'off',
       'quote-props': 'off',
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      'quotes': 'off', // superseded by @stylistic/quotes
       'spaced-comment': 'off',
       'indent': 'off',
       'no-unused-vars': 'off',
@@ -132,6 +132,17 @@ export default defineConfig(
       '@typescript-eslint/no-unused-expressions': 'off',
       'chai-friendly/no-unused-expressions': ['error', { 'allowShortCircuit': true }],
       '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
+      'no-invalid-this': 'error',
+      'require-await': 'off', // superseded by @typescript-eslint/require-await
+
+      '@typescript-eslint/no-unsafe-call': 'off', // function call to fn with `any` type
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'prefer-promise-reject-errors': 'off',
+      '@typescript-eslint/prefer-promise-reject-errors': 'off',
 
       '@stylistic/indent': ['error', 2, { 'SwitchCase': 1 }],
       '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],

src/cleartext.js
@@ -209,8 +209,9 @@ function verifyHeaders(headers, packetlist) {
  * @param {Object} options
  * @param {String} options.text
  * @static
- * @async
+ * @async not necessary, but needed to align with createMessage
  */
+// eslint-disable-next-line @typescript-eslint/require-await
 export async function createCleartextMessage({ text, ...rest }) {
   if (!text) {
     throw new Error('createCleartextMessage: must pass options object containing `text`');

src/crypto/aes_kw.js
@@ -31,7 +31,7 @@ const webCrypto = util.getWebCrypto();
  * @param {enums.symmetric.aes128|enums.symmetric.aes256|enums.symmetric.aes192} algo - AES algo
  * @param {Uint8Array} key - wrapping key
  * @param {Uint8Array} dataToWrap
- * @returns {Uint8Array} wrapped key
+ * @returns {Promise<Uint8Array>} wrapped key
  */
 export async function wrap(algo, key, dataToWrap) {
   const { keySize } = getCipherParams(algo);
@@ -63,7 +63,7 @@ export async function wrap(algo, key, dataToWrap) {
  * @param {enums.symmetric.aes128|enums.symmetric.aes256|enums.symmetric.aes192} algo - AES algo
  * @param {Uint8Array} key - wrapping key
  * @param {Uint8Array} wrappedData
- * @returns {Uint8Array} unwrapped data
+ * @returns {Promise<Uint8Array>} unwrapped data
  */
 export async function unwrap(algo, key, wrappedData) {
   const { keySize } = getCipherParams(algo);

src/crypto/biginteger.ts
@@ -162,7 +162,7 @@ export function bitLength(x: bigint) {
   const target = x < _0n ? BigInt(-1) : _0n;
   let bitlen = 1;
   let tmp = x;
-  // eslint-disable-next-line no-cond-assign
+
   while ((tmp >>= _1n) !== target) {
     bitlen++;
   }
@@ -177,7 +177,7 @@ export function byteLength(x: bigint) {
   const _8n = BigInt(8);
   let len = 1;
   let tmp = x;
-  // eslint-disable-next-line no-cond-assign
+
   while ((tmp >>= _8n) !== target) {
     len++;
   }

src/crypto/cipher/twofish.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-mixed-operators, no-fallthrough */
-
-
 /* Modified by Recurity Labs GmbH
  *
  * Cipher.js
@@ -98,6 +95,7 @@ function createTwofish() {
     ];
     const ror4 = [0, 8, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15];
     const ashx = [0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12, 5, 14, 7];
+    /** @type {number[][]} */
     const q = [
       [],
       []
@@ -158,11 +156,13 @@ function createTwofish() {
           b = q[0][b] ^ getB(key[3], 1);
           c = q[0][c] ^ getB(key[3], 2);
           d = q[1][d] ^ getB(key[3], 3);
+        // eslint-disable-next-line no-fallthrough
         case 3:
           a = q[1][a] ^ getB(key[2], 0);
           b = q[1][b] ^ getB(key[2], 1);
           c = q[0][c] ^ getB(key[2], 2);
           d = q[0][d] ^ getB(key[2], 3);
+        // eslint-disable-next-line no-fallthrough
         case 2:
           a = q[0][q[0][a] ^ getB(key[1], 0)] ^ getB(key[0], 0);
           b = q[0][q[1][b] ^ getB(key[1], 1)] ^ getB(key[0], 1);
@@ -222,11 +222,13 @@ function createTwofish() {
           b = q[0][b] ^ getB(sKey[3], 1);
           c = q[0][c] ^ getB(sKey[3], 2);
           d = q[1][d] ^ getB(sKey[3], 3);
+        // eslint-disable-next-line no-fallthrough
         case 3:
           a = q[1][a] ^ getB(sKey[2], 0);
           b = q[1][b] ^ getB(sKey[2], 1);
           c = q[0][c] ^ getB(sKey[2], 2);
           d = q[0][d] ^ getB(sKey[2], 3);
+        // eslint-disable-next-line no-fallthrough
         case 2:
           tfsM[0][i] = m[0][q[0][q[0][a] ^ getB(sKey[1], 0)] ^ getB(sKey[0], 0)];
           tfsM[1][i] = m[1][q[0][q[1][b] ^ getB(sKey[1], 1)] ^ getB(sKey[0], 1)];

src/crypto/cipherMode/cfb.js
@@ -49,11 +49,10 @@ const nodeAlgos = {
  * See {@link https://tools.ietf.org/html/rfc4880#section-9.2|RFC 4880 9.2} for algorithms.
  * @param {module:enums.symmetric} algo - Symmetric encryption algorithm
  * @returns {Promise<Uint8Array>} Random bytes with length equal to the block size of the cipher, plus the last two bytes repeated.
- * @async
  */
-export async function getPrefixRandom(algo) {
+export function getPrefixRandom(algo) {
   const { blockSize } = getCipherParams(algo);
-  const prefixrandom = await getRandomBytes(blockSize);
+  const prefixrandom = getRandomBytes(blockSize);
   const repeat = new Uint8Array([prefixrandom[prefixrandom.length - 2], prefixrandom[prefixrandom.length - 1]]);
   return util.concat([prefixrandom, repeat]);
 }
@@ -156,7 +155,10 @@ class WebCryptoEncryptor {
     this.zeroBlock = new Uint8Array(this.blockSize);
   }
 
-  static async isSupported(algo) {
+  /**
+   * @returns {Promise<boolean>}
+   */
+  static isSupported(algo) {
     const { keySize } = getCipherParams(algo);
     return webCrypto.importKey('raw', new Uint8Array(keySize), 'aes-cbc', false, ['encrypt'])
       .then(() => true, () => false);
@@ -283,6 +285,7 @@ class NobleStreamProcessor {
     return dst;
   }
 
+  // eslint-disable-next-line @typescript-eslint/require-await
   async processChunk(value) {
     const missing = this.nextBlock.length - this.i;
     const added = value.subarray(0, missing);
@@ -321,6 +324,7 @@ class NobleStreamProcessor {
     return processedBlock;
   }
 
+  // eslint-disable-next-line @typescript-eslint/require-await
   async finish() {
     let result;
     if (this.i === 0) { // nothing more to encrypt
@@ -354,7 +358,7 @@ async function aesEncrypt(algo, key, pt, iv) {
   return nobleAesCfb(key, iv).encrypt(pt);
 }
 
-async function aesDecrypt(algo, key, ct, iv) {
+function aesDecrypt(algo, key, ct, iv) {
   if (util.isStream(ct)) {
     const cfb = new NobleStreamProcessor(false, algo, key, iv);
     return streamTransform(ct, value => cfb.processChunk(value), () => cfb.finish());

commit 6da0ca2d0a8cbb710d954423b92652a654488c19

TS: fix AnyPacket declaration to also include `BasePacket<true>` subclasses
Issue introduced in https://github.com/openpgpjs/openpgpjs/commit/48d1cfd15f3cfe37c2bcf0666060c0ec460b57a6 due to `BasePacket` defaulting to `BasePacket<false>`

openpgp.d.ts
@@ -540,7 +540,7 @@ export class UnparseablePacket {
   write: () => Uint8Array;
 }
 
-export type AnyPacket = BasePacket | UnparseablePacket;
+export type AnyPacket = BasePacket<boolean> | UnparseablePacket;
 export type AnySecretKeyPacket = SecretKeyPacket | SecretSubkeyPacket;
 export type AnyKeyPacket = BasePublicKeyPacket;
 

test/typescript/definitions.ts
@@ -15,7 +15,7 @@ import {
   readMessage, createMessage, Message, createCleartextMessage,
   encrypt, decrypt, sign, verify, config, enums,
   generateSessionKey, encryptSessionKey, decryptSessionKeys,
-  LiteralDataPacket, PacketList, CompressedDataPacket, PublicKeyPacket, PublicSubkeyPacket, SecretKeyPacket, SecretSubkeyPacket, CleartextMessage,
+  LiteralDataPacket, PacketList, CompressedDataPacket, SymEncryptedIntegrityProtectedDataPacket, PublicKeyPacket, PublicSubkeyPacket, SecretKeyPacket, SecretSubkeyPacket, CleartextMessage,
   WebStream, NodeWebStream
 } from 'openpgp';
 
@@ -104,6 +104,8 @@ import {
   expect(encryptedBinaryObject).to.be.instanceOf(Message);
   const encryptedTextObject: Message<string> = await encrypt({ encryptionKeys: publicKeys, message: textMessage, format: 'object' });
   expect(encryptedTextObject).to.be.instanceOf(Message);
+  const seipdPacket = encryptedTextObject.packets.findPacket(enums.packet.symEncryptedIntegrityProtectedData) as SymEncryptedIntegrityProtectedDataPacket;
+  expect(seipdPacket).to.be.instanceOf(SymEncryptedIntegrityProtectedDataPacket);
 
   // Session key functions
   // Get session keys from encrypted message