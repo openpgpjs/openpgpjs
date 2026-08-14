18d34a478c328936c751ce88684b5c19e1864454
larabr <7375870+larabr@users.noreply.github.com>
2026-08-14 14:18:22 +0200

    Add `test-browser:no-webkit` command
    Since the inline --browsers gives not customization options

--- test/web-test-runner.config.js
+++ test/web-test-runner.config.js
@@ -1,3 +1,21 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
+const sharedPlaywrightCIOptions = {
+  // createBrowserContext: ({ browser }) => browser.newContext({ ignoreHTTPSErrors: true }),
+  headless: true
+};
+
+const commonBrowsers = [
+  playwrightLauncher({
+    ...sharedPlaywrightCIOptions,
+    product: 'chromium'
+  }),
+  playwrightLauncher({
+    ...sharedPlaywrightCIOptions,
+    product: 'firefox'
+  })
+];
+
 export default {
   nodeResolve: true, // to resolve npm module imports in `unittests.html`
   files: './test/unittests.html',
@@ -9,5 +27,22 @@ export default {
   concurrentBrowsers: 3,
   concurrency: 1, // see https://github.com/modernweb-dev/web/issues/2706
   coverage: false,
-  groups: []
+  groups: [
+    { name: 'local' }, // group meant to be used with either --browser or --manual options via CLI
+    {
+      name: 'headless:ci',
+      browsers: [
+        ...commonBrowsers,
+        playwrightLauncher({
+          ...sharedPlaywrightCIOptions,
+          product: 'webkit'
+        })
+      ]
+    },
+    {
+      // WebKit is skipped on Windows (see .github/workflows/tests.yml)
+      name: 'headless:ci:no-webkit',
+      browsers: commonBrowsers
+    }
+  ]
 };
